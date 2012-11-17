0572f0390b57e015a98c212374db2f10a626c1cc
Imad Nadkar <imadnadkar@gmail.com>
2012-11-17 14:54:48 +0530

    Importing the LabManSys project

---
+++ src/main/java/com/jcs/labmansys/App.java
@@ -0,0 +1,13 @@
+package com.jcs.labmansys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
